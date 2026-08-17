0fcf36998afa14396c3af5835011dad108a873e6
yu2971512385-ui <yu2971512385@gmail.com>
2026-08-17 18:44:01 +0800

    Publish sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "score-atlas-v2";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./data.js", "./manifest.webmanifest", "./assets/icon.svg", "./assets/lucide.min.js"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match("./index.html")))
+  );
+});
