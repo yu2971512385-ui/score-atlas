4f4e906d8b5a0ddf7c40c8d918879f5114991e21
yu2971512385-ui <yu2971512385@gmail.com>
2026-08-18 13:42:37 +0800

    Publish tunes.js

---
+++ tunes.js
@@ -0,0 +1,244 @@
+window.SCORE_ATLAS_TUNES = [
+  {
+    id: "ode-to-joy",
+    title: "欢乐颂",
+    original: "Ode to Joy",
+    aliases: ["贝多芬第九交响曲", "An die Freude"],
+    composer: "路德维希·凡·贝多芬",
+    era: "1824",
+    rights: "公共领域旋律 · 单声部教学转写",
+    sourceUrl: "https://en.wikipedia.org/wiki/Ode_to_Joy",
+    abc: `X:1
+T:欢乐颂
+C:L. v. Beethoven
+M:4/4
+L:1/4
+Q:1/4=104
+K:C
+E E F G | G F E D | C C D E | E3 D | D4 |
+E E F G | G F E D | C C D E | D3 C | C4 |
+D D E C | D E/2F/2 E C | D E/2F/2 E D | C D G,2 |
+E E F G | G F E D | C C D E | D3 C | C4 |]`
+  },
+  {
+    id: "twinkle",
+    title: "小星星",
+    original: "Ah! vous dirai-je, maman",
+    aliases: ["Twinkle Twinkle Little Star", "一闪一闪亮晶晶"],
+    composer: "法国传统旋律",
+    era: "18 世纪",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Twinkle,_Twinkle,_Little_Star",
+    abc: `X:1
+T:小星星
+C:French traditional
+M:4/4
+L:1/4
+Q:1/4=96
+K:C
+C C G G | A A G2 | F F E E | D D C2 |
+G G F F | E E D2 | G G F F | E E D2 |
+C C G G | A A G2 | F F E E | D D C2 |]`
+  },
+  {
+    id: "frere-jacques",
+    title: "两只老虎 / 雅克兄弟",
+    original: "Frère Jacques",
+    aliases: ["两只老虎", "Brother John", "Are You Sleeping"],
+    composer: "法国传统童谣",
+    era: "传统",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Fr%C3%A8re_Jacques",
+    abc: `X:1
+T:两只老虎 / Frère Jacques
+C:French traditional
+M:4/4
+L:1/4
+Q:1/4=112
+K:C
+C D E C | C D E C | E F G2 | E F G2 |
+G/2A/2 G/2F/2 E C | G/2A/2 G/2F/2 E C | C G, C2 | C G, C2 |]`
+  },
+  {
+    id: "mary-lamb",
+    title: "玛丽有只小羊羔",
+    original: "Mary Had a Little Lamb",
+    aliases: ["Mary's Little Lamb"],
+    composer: "美国传统童谣",
+    era: "19 世纪",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Mary_Had_a_Little_Lamb",
+    abc: `X:1
+T:玛丽有只小羊羔
+C:American traditional
+M:4/4
+L:1/4
+Q:1/4=104
+K:C
+E D C D | E E E2 | D D D2 | E G G2 |
+E D C D | E E E E | D D E D | C4 |]`
+  },
+  {
+    id: "london-bridge",
+    title: "伦敦桥",
+    original: "London Bridge Is Falling Down",
+    aliases: ["London Bridge"],
+    composer: "英国传统童谣",
+    era: "传统",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/London_Bridge_Is_Falling_Down",
+    abc: `X:1
+T:伦敦桥
+C:English traditional
+M:4/4
+L:1/4
+Q:1/4=112
+K:C
+G A G F | E F G2 | D E F2 | E F G2 |
+G A G F | E F G2 | D G E C | C4 |]`
+  },
+  {
+    id: "happy-birthday",
+    title: "生日快乐",
+    original: "Happy Birthday to You",
+    aliases: ["祝你生日快乐", "Good Morning to All"],
+    composer: "Patty Hill / Mildred J. Hill",
+    era: "1893",
+    rights: "旋律已进入公共领域 · 单声部教学转写",
+    sourceUrl: "https://en.wikipedia.org/wiki/Happy_Birthday_to_You",
+    abc: `X:1
+T:生日快乐
+C:Patty Hill / Mildred J. Hill
+M:3/4
+L:1/8
+Q:1/4=92
+K:C
+G>G | A2 G2 c2 | B4 G>G | A2 G2 d2 | c4 G>G |
+g2 e2 c2 | B2 A2 f>f | e2 c2 d2 | c4 |]`
+  },
+  {
+    id: "jingle-bells",
+    title: "铃儿响叮当",
+    original: "Jingle Bells",
+    aliases: ["One Horse Open Sleigh"],
+    composer: "James Lord Pierpont",
+    era: "1857",
+    rights: "公共领域旋律 · 单声部教学转写",
+    sourceUrl: "https://en.wikipedia.org/wiki/Jingle_Bells",
+    abc: `X:1
+T:铃儿响叮当
+C:James Lord Pierpont
+M:4/4
+L:1/4
+Q:1/4=116
+K:C
+E E E2 | E E E2 | E G C D | E4 |
+F F F F | F E E E/2E/2 | E D D E | D2 G2 |
+E E E2 | E E E2 | E G C D | E4 |
+F F F F | F E E E/2E/2 | G G F D | C4 |]`
+  },
+  {
+    id: "amazing-grace",
+    title: "奇异恩典",
+    original: "Amazing Grace",
+    aliases: ["Amazing Grace How Sweet the Sound"],
+    composer: "英国 / 美国传统圣歌旋律",
+    era: "19 世纪",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Amazing_Grace",
+    abc: `X:1
+T:奇异恩典
+C:Traditional hymn tune
+M:3/4
+L:1/4
+Q:1/4=76
+K:G
+D | G2 B/2G/2 | B2 A | G2 E | D2 D |
+G2 B/2G/2 | B2 A | d2 B | d3 |
+B2 d | B2 G | E2 G/2E/2 | D2 D |
+G2 B/2G/2 | B2 A | G3 |]`
+  },
+  {
+    id: "greensleeves",
+    title: "绿袖子",
+    original: "Greensleeves",
+    aliases: ["What Child Is This"],
+    composer: "英国传统旋律",
+    era: "16 世纪",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Greensleeves",
+    abc: `X:1
+T:绿袖子
+C:English traditional
+M:6/8
+L:1/8
+Q:3/8=70
+K:Am
+A2 | c3 d2 e | f3 e2 d | B3 G2 A | B3 A4 |
+c3 d2 e | f3 e2 d | B3 G2 ^G | A3 A4 |
+g3 g2 f | e3 c2 d | e3 e2 d | c3 B4 |
+G3 G2 A | B3 G2 ^G | A3 A4 |]`
+  },
+  {
+    id: "auld-lang-syne",
+    title: "友谊地久天长",
+    original: "Auld Lang Syne",
+    aliases: ["蛍の光", "旧日时光"],
+    composer: "苏格兰传统旋律",
+    era: "18 世纪",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Auld_Lang_Syne",
+    abc: `X:1
+T:友谊地久天长
+C:Scottish traditional
+M:4/4
+L:1/8
+Q:1/4=92
+K:G
+D2 | G3 G G2 B2 | A3 G A2 B2 | G3 G B2 d2 | e6 e2 |
+d3 B B2 G2 | A3 G A2 B2 | G3 E E2 D2 | G6 e2 |
+d3 B B2 G2 | A3 G A2 e2 | d3 B B2 d2 | e6 g2 |
+d3 B B2 G2 | A3 G A2 B2 | G3 E E2 D2 | G6 |]`
+  },
+  {
+    id: "sakura",
+    title: "樱花",
+    original: "さくらさくら / Sakura Sakura",
+    aliases: ["Sakura", "樱花樱花"],
+    composer: "日本传统旋律",
+    era: "江户时代",
+    rights: "传统旋律 · 公共领域",
+    sourceUrl: "https://en.wikipedia.org/wiki/Sakura_Sakura",
+    abc: `X:1
+T:樱花
+C:Japanese traditional
+M:4/4
+L:1/4
+Q:1/4=72
+K:Am
+A A B2 | A A B2 | A B c B | A B/2A/2 F2 |
+E C E F | E E C2 | B, B, C2 | B, B, C2 |
+B, C E F | E C B,2 | A, C B, A, | A,4 |]`
+  },
+  {
+    id: "jasmine-flower",
+    title: "茉莉花",
+    original: "Mo Li Hua",
+    aliases: ["Jasmine Flower", "好一朵美丽的茉莉花"],
+    composer: "中国传统民歌",
+    era: "清代已有记谱",
+    rights: "传统旋律 · 公共领域；采用常见江苏旋律的单声部教学转写",
+    sourceUrl: "https://imslp.org/wiki/Jasmine_Flower_%28Folk_Songs%2C_Chinese%29",
+    abc: `X:1
+T:茉莉花
+C:Chinese traditional
+M:4/4
+L:1/4
+Q:1/4=84
+K:C
+E E G A | c c A G | G A G2 | E E G A |
+c c A G | G A G2 | G G E G | A A G2 |
+E D E G | E D C2 | C E G A | c A G E |
+D E D2 | C4 |]`
+  }
+].map(tune => ({ ...tune, reviewedAt: "2026-08-18" }));
