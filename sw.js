5e90de24567e5c546019af567198a50d32d68a1d
yu2971512385-ui <yu2971512385@gmail.com>
2026-08-19 12:28:35 +0800

    Publish sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,31 @@
-const CACHE = "score-atlas-v5-0";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=5.0", "./mascot.css?v=5.0", "./app.js?v=5.0", "./arranger.js?v=5.0", "./audio-transcriber.js?v=5.0", "./mascot.js?v=5.0", "./tunes.js?v=5.0", "./data.js?v=5.0", "./manifest.webmanifest", "./assets/icon.svg", "./assets/lucide.min.js", "./assets/abcjs-basic-min.js?v=5.0"];
+const CACHE = "score-atlas-v5-1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css?v=5.1",
+  "./kirby-theme.css?v=5.1",
+  "./kirby-theme.js?v=5.1",
+  "./app.js?v=5.1",
+  "./arranger.js?v=5.1",
+  "./audio-transcriber.js?v=5.1",
+  "./tunes.js?v=5.1",
+  "./data.js?v=5.1",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/lucide.min.js",
+  "./assets/abcjs-basic-min.js?v=5.1",
+  "./assets/kirby-wallpaper-drive.jpg",
+  "./assets/kirby-wallpaper-squish.jpg",
+  "./assets/kirby-wallpaper-space.jpg",
+  "./assets/kirby-loader-01.jpg",
+  "./assets/kirby-loader-02.jpg",
+  "./assets/kirby-loader-03.jpg",
+  "./assets/kirby-loader-04.jpg",
+  "./assets/kirby-loader-05.jpg",
+  "./assets/kirby-loader-06.jpg",
+  "./assets/kirby-loader-07.jpg",
+  "./assets/kirby-loader-08.jpg"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
